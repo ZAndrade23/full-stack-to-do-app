a4a5fd160009541d60dc7838cfb2cc9c318dcb3d
Zach Andrade <zach_andrade@yahoo.com>
2023-11-01 00:59:45 -0500

    I finished putting in the axios.put and made a toggle button that imakes the characters green if they are main characters and grey if they are side characters. I need to update the css a lot and make the project look better now. also add a toggle to add a main character selction.

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -32,7 +32,17 @@ router.post('/', (req, res)=>
     })
 })
 // PUT
-
+router.put('/:id', (req, res) => {
+    console.log('PUT /character', req.params);
+    const queryText = 'UPDATE "characters" SET "main" = NOT "main" WHERE "id" = $1;';
+    pool.query(queryText, [req.params.id])
+    .then((result) => {
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('Error in PUT', error);
+        res.sendStatus(500);
+    })
+})
 // DELETE
 router.delete('/:id', (req,res) => {
 console.log('DELETE /character', req.params);
--- src/components/CharacterItem/CharacterItem.jsx
+++ src/components/CharacterItem/CharacterItem.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import './CharacterItem.css';
 function CharacterItem(props) {
     const clickHandler = () => {
         axios.delete(`/character/${props.character.id}`).then((response) => {
@@ -8,13 +8,24 @@ function CharacterItem(props) {
             console.error(error);
             alert('something went wrong!');
         })
-    
-    
+    }
+    const toggleMain = () => {
+        // toggle creature property of immortal
+        axios.put(`/character/${props.character.id}`).then((response) => {
+            // After edit, refresh the list
+            props.getCharacterList();
+        }).catch((error)=> {
+            console.error(error);
+            alert('SOmething went wrong!');
+        })
+        
+
     }
     return (
-        <li>
+        <li className={ props.character.main ? 'highlight' : 'normal'}>
             {props.character.name} is from the anime {props.character.anime}
             <button onClick={clickHandler}> Instant Transmit </button>
+            <button onClick={toggleMain}> Toggle </button>
         </li>
 
     )
